0b31810b321c908400a13cf31f318807ab3accfb
Kian <wish1076@naver.com>
2025-07-19 15:36:41 +0900

    feat : 회원가입(POST /signup) 테스트 케이스 작성

---
+++ subproject/boot/src/test/kotlin/com/example/SignUpHttpTest.kt
@@ -0,0 +1,66 @@
+package com.example
+import com.example.protopie.DatabaseConfiguration
+import com.example.protopie.UserEntity
+import com.example.protopie.dto.SignUpRequest
+import io.kotest.core.spec.style.FreeSpec
+import io.ktor.client.request.*
+import io.ktor.http.*
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.transactions.transaction
+import kotlin.test.assertEquals
+
+class SignUpHttpTest:FreeSpec ({
+
+    lateinit var databaseConfiguration: DatabaseConfiguration
+    beforeTest {
+        databaseConfiguration = HttpTestEnvironment.global.databaseConfiguration
+    }
+
+    "회원 가입 테스트" - {
+        "성공 케이스" {
+            runCustomTestApplication(databaseConfiguration){
+                val response = client.post("/signup") {
+                    contentType(ContentType.Application.Json)
+
+                    val request = SignUpRequest(
+                        TestDataHelper.generateEmail(),
+                        "testUsername",
+                        "password",
+                    )
+
+                    setBody(request)
+                }
+                assertEquals(HttpStatusCode.NoContent, response.status)
+            }
+        }
+        "실패 케이스" - {
+            "이메일 중복 실패"-{
+                runCustomTestApplication(databaseConfiguration){
+                    val existedEmail = TestDataHelper.generateEmail()
+                    transaction {
+                        UserEntity.insert {
+                            it[email] = existedEmail
+                            it[username] = "testUsername"
+                            it[password] = "<PASSWORD>"
+                        }
+                    }
+
+                    val response = client.post("/signup") {
+                        contentType(ContentType.Application.Json)
+
+                        val request = SignUpRequest(
+                            existedEmail,
+                            "testUsername",
+                            "password",
+                        )
+
+                        setBody(request)
+                    }
+
+                    assertEquals(HttpStatusCode.Conflict, response.status)
+                }
+            }
+        }
+    }
+})
+
